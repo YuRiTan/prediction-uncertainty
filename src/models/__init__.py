9033f0c3c6c939a465f32e347a6872e989d45ea0
Yu Ri Tan <yuri_tan@outlook.com>
2019-10-25 11:47:21 +0200

    updated bayesian linear regression

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
-from mcdqrnn import DeepQuantileRegression
-from quantile_utils import create_quantiles, get_quantile_pred
+from .mcdqrnn import DeepQuantileRegression
+from .quantile_utils import create_quantiles, get_quantile_pred
+from .blr import BayesianLinearRegression
